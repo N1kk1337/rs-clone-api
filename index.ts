8a534ad7052f413cc19edcd530bfa308e41ba919
Nikki NoLife <nikkinolife@gmail.com>
2023-02-06 20:01:17 +0400

    fix: error handling

--- index.ts
+++ index.ts
@@ -5,13 +5,20 @@ const server = jsonServer.create();
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
-const PORT = 3000;
+const PORT = 3004;
 
 server.use(middlewares);
 
 
+// server.get('/users', (req, res) => {
 
+//     const { id } = req.query;
 
+//     if (!db.users.find(user => user.id === +id!)) {
+//         return res.status(404).send('User with such id was not found')
+//     }
+//     }
+// )
 
 server.use(router);
 server.listen(PORT, () => {
