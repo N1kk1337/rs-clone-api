64d27b665ecf8f652a0e9b78e16f7d198828b46d
Nikki NoLife <nikkinolife@gmail.com>
2023-02-07 09:19:14 +0400

    feat: add logger

--- index.ts
+++ index.ts
@@ -9,18 +9,30 @@ const PORT = 3004;
 
 server.use(middlewares);
 
+const fs = require('fs');
+server.use((req, res, next) => {
+    const log = `${new Date().toISOString()} ${req.method} ${req.url}`;
+    // что ещё писать в логи?
+    fs.appendFileSync('log.txt', log + '\n');
+  
+    next();
+  });
 
-// server.get('/users', (req, res) => {
-
-//     const { id } = req.query;
-
-//     if (!db.users.find(user => user.id === +id!)) {
-//         return res.status(404).send('User with such id was not found')
+// server.use((req, res, next) => {
+//     if (req.method === 'GET' && req.url.match(/\/users\/\d+$/)) {
+//       const userId = parseInt(req.url.split('/').pop()!);
+//       const user = router.db.get('users').find({ id: userId }).value();
+//       if (!user) {
+//         res.status(404).send({ error: 'User with such id was not found' });
+//         return;
+//       }
 //     }
-//     }
-// )
+//     next();
+//   });
+
 
 server.use(router);
+
 server.listen(PORT, () => {
     console.log('Server is running on port', PORT);
 });
